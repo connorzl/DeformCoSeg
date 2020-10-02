d8a93e1aefef7806ec854e45d197bb2c041ab219
connorzl <connorzl@stanford.edu>
2020-10-01 20:21:24 -0700

    code cleanup -- import pointnetae better

--- src/python/cad_neural_deform_pointnetae_multi.py
+++ src/python/cad_neural_deform_pointnetae_multi.py
@@ -5,17 +5,13 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + 'layers')
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + 'util')
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'build')))
 
-POINTNET_AE_DIR = "../../../../pytorch_shapenet_ae/exps/exp_baseline1"
-POINTNET_AE_UTILS_DIR = "../../../../pytorch_shapenet_ae/exps/utils"
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), POINTNET_AE_DIR))
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), POINTNET_AE_UTILS_DIR))
 
 import torch.optim as optim
 import torch
 from layers.graph_loss_layer import GraphLossLayerMulti
 from layers.reverse_loss_layer import ReverseLossLayer
 from layers.neuralode_conditional import NeuralFlowDeformer
-from models.model_v1 import Network
+from layers.pointnet_ae import Network
 from util.samplers import load_mesh
 import pyDeform
 
---
+++ src/python/layers/pointnet_ae.py
@@ -0,0 +1,124 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import sys, os
+
+
+class PointNet(nn.Module):
+    def __init__(self):
+        super(PointNet, self).__init__()
+        
+        self.conv1 = nn.Conv1d(3, 64, 1)
+        self.conv2 = nn.Conv1d(64, 128, 1)
+        self.conv3 = nn.Conv1d(128, 256, 1)
+        self.conv4 = nn.Conv1d(256, 1024, 1)
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(256)
+        self.bn4 = nn.BatchNorm1d(1024)
+
+        self.fc1 = nn.Linear(1024, 1024)
+        self.bn5 = nn.BatchNorm1d(1024)
+
+    """
+        Input: B x N x 3
+        Output: B x F
+    """
+    def forward(self, pcs):
+        net = pcs.permute(0, 2, 1)
+
+        net = torch.relu(self.bn1(self.conv1(net)))
+        net = torch.relu(self.bn2(self.conv2(net)))
+        net = torch.relu(self.bn3(self.conv3(net)))
+        net = torch.relu(self.bn4(self.conv4(net)))
+
+        net = net.max(dim=-1)[0]
+
+        net = torch.relu(self.bn5(self.fc1(net)))
+        
+        return net
+
+
+class FCDecoder(nn.Module):
+
+    def __init__(self, num_point=2048):
+        super(FCDecoder, self).__init__()
+        print('Using FCDecoder-NoBN!')
+
+        self.mlp1 = nn.Linear(1024, 1024)
+        self.mlp2 = nn.Linear(1024, 1024)
+        self.mlp3 = nn.Linear(1024, num_point*3)
+
+    def forward(self, feat):
+        batch_size = feat.shape[0]
+
+        net = feat
+        net = torch.relu(self.mlp1(net))
+        net = torch.relu(self.mlp2(net))
+        net = self.mlp3(net).view(batch_size, -1, 3)
+
+        return net
+
+
+class FCUpconvDecoder(nn.Module):
+
+    def __init__(self, num_point=2048):
+        super(FCUpconvDecoder, self).__init__()
+        print('Using FCUpconvDecoder-NoBN!')
+
+        self.mlp1 = nn.Linear(1024, 1024)
+        self.mlp2 = nn.Linear(1024, 1024)
+        self.mlp3 = nn.Linear(1024, 1024*3)
+
+        self.deconv1 = nn.ConvTranspose2d(1024, 1024, 2, 1)
+        self.deconv2 = nn.ConvTranspose2d(1024, 512, 3, 1)
+        self.deconv3 = nn.ConvTranspose2d(512, 256, 4, 2)
+        self.deconv4 = nn.ConvTranspose2d(256, 128, 5, 3)
+        self.deconv5 = nn.ConvTranspose2d(128, 3, 1, 1)
+
+    def forward(self, feat):
+        batch_size = feat.shape[0]
+
+        fc_net = feat
+        fc_net = torch.relu(self.mlp1(fc_net))
+        fc_net = torch.relu(self.mlp2(fc_net))
+        fc_net = self.mlp3(fc_net).view(batch_size, -1, 3)
+
+        upconv_net = feat.view(batch_size, -1, 1, 1)
+        upconv_net = torch.relu(self.deconv1(upconv_net))
+        upconv_net = torch.relu(self.deconv2(upconv_net))
+        upconv_net = torch.relu(self.deconv3(upconv_net))
+        upconv_net = torch.relu(self.deconv4(upconv_net))
+        upconv_net = self.deconv5(upconv_net).view(batch_size, 3, -1).permute(0, 2, 1)
+        
+        net = torch.cat([fc_net, upconv_net], dim=1)
+
+        return net
+
+
+class Network(nn.Module):
+
+    def __init__(self, conf):
+        super(Network, self).__init__()
+        self.conf = conf
+
+        self.encoder = PointNet()
+
+        if conf.decoder_type == 'fc':
+            self.decoder = FCDecoder(num_point=conf.num_point)
+
+        elif conf.decoder_type == 'fc_upconv':
+            self.decoder = FCUpconvDecoder(num_point=conf.num_point)
+
+        else:
+            raise ValueError('ERROR: unknown decoder_type %s!' % decoder_type)
+
+    """
+        Input: B x N x 3
+        Output: B x N x 3, B x F
+    """
+    def forward(self, input_pcs):
+        feats = self.encoder(input_pcs)
+        output_pcs = self.decoder(feats)
+        return output_pcs, feats  
